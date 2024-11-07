e4e5f0d846d09827f6206dfd5bdf6b6908d58957
Dahkenangnon <dah.kenangnon@gmail.com>
2024-11-07 06:12:31 +0100

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {" "}
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="text-center text-semibold">
+        <p>My GitHub page is under reconstructions...</p>
+      </div>
+      </div>
+   
+    </>
+  );
+}
+
+export default App;
